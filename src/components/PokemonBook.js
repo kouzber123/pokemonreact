51ba9a8a183cb01184b8c168d33c1dd0926cf117
kouzber123 <48252892+kouzber123@users.noreply.github.com>
2022-06-01 15:52:05 +0300

    sheesh

--- src/components/PokemonBook.js
+++ src/components/PokemonBook.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 function PokemonBook() {
-  var flavor_skill_text = [];
-  var skill_namess = [];
   const [isSearchDone, setIsSearchDone] = useState(Boolean);
   const [list, setList] = useState([]);
+  const [name, setName] = useState([]);
+  const [abilities, setAbilities] = useState([]);
+  const [pokemon, setPokemon] = useState([]);
 
+  //first fetch gets the 20 pokemons and its basic data
   useEffect(() => {
     const url = `https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20`;
     const fetchData = async () => {
@@ -13,6 +15,7 @@ function PokemonBook() {
         const response = await fetch(url);
         const json = await response.json();
 
+        //put the basic data into array
         setList([json.results]);
         setIsSearchDone(true);
       } catch (error) {
@@ -23,8 +26,9 @@ function PokemonBook() {
   }, []);
   console.log(list);
 
+  // next get pokemon ability names
   useEffect(() => {
-    //1) get pokemon details
+    var nameArray = [];
     const getDetails = async () => {
       try {
         axios.all(
@@ -33,8 +37,38 @@ function PokemonBook() {
             return axios.get(u).then(response => {
               if (response) {
                 const data = response.data;
-                // console.log(data);
-                getSkillMeaning(data.abilities);
+
+                axios.all(
+                  data.abilities.map(async url => {
+                    var skillUrl = url.ability.url;
+                    //returns one skill synopsis at the time
+                    return axios.get(skillUrl).then(response => {
+                      //array
+                      if (response) {
+                        var res = response.data.flavor_text_entries[1].flavor_text;
+                        nameArray.push(res);
+                      }
+
+                      // setPokemon(arr => [...arr, newVal]);
+                    });
+                  })
+                );
+                //--------
+
+                var d = data.abilities.map(item => {
+                  return item.ability.name + " " + nameArray[i];
+                });
+
+                // console.log(d);
+                setName(arr => [...arr, d]);
+                var newVal = {
+                  id: data.id,
+                  name: data.name,
+                  abilities: d,
+                  spriteFront: data.sprites.front_default,
+                  spriteBack: data.sprites.back_default,
+                  i: 0
+                };
               }
             });
           })
@@ -43,42 +77,9 @@ function PokemonBook() {
         console.log(error);
       }
     };
-    //get pokemon skill details
-    const getSkillMeaning = async data => {
-      //get ability names to combine
-      // console.log(data);
-      var skill_names = data.map(item => {
-        skill_namess.push(item.ability.name);
-        return item.ability.name;
-      });
-
-      // console.log(skill_names);
-      try {
-        axios.all(
-          data.map(async url => {
-            var skillUrl = url.ability.url;
-            return axios.get(skillUrl).then(response => {
-              if (response) {
-                var res = response.data.flavor_text_entries[1].flavor_text;
-                flavor_skill_text.push(res);
-              }
-              var j = -1;
-              var combine_Skill_synopsis = skill_namess.map(skill => {
-                j++;
-                return skill + " " + flavor_skill_text[j];
-              });
-              console.log(combine_Skill_synopsis);
-            });
-          })
-        );
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
     getDetails();
-  }, [list, skill_namess, flavor_skill_text]);
-
+  }, []);
+  // console.log(name);
   return (
     <div>
       <h1>hello</h1>
@@ -87,3 +88,37 @@ function PokemonBook() {
 }
 
 export default PokemonBook;
+
+//thirdly get summary > meanings of the skills
+// const getSkillMeaning = async (data, abilities) => {
+//   //get ability names to combine
+//   // console.log(data);
+//   abilities.map(item => {
+//     skill_namess.push(item.ability.name);
+//     return item.ability.name;
+//   });
+
+//   // console.log(skill_names);
+//   try {
+//     axios.all(
+//       abilities.map(async url => {
+//         var skillUrl = url.ability.url;
+//         return axios.get(skillUrl).then(response => {
+//           if (response) {
+//             var res = response.data.flavor_text_entries[1].flavor_text;
+//             flavor_skill_text.push(res);
+//           }
+//           //combine skill name and summary of it
+//           var j = -1;
+//           var combine_Skill_synopsis = skill_namess.map(skill => {
+//             j++;
+//             return skill + " " + flavor_skill_text[j];
+//           });
+
+//         });
+//       })
+//     );
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
